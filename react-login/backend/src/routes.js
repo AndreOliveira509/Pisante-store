3ef264d73462a50642ba852d78689a0b1324d999
André Oliveira <andrelitoscria@gmail.com>
2025-06-04 22:34:20 -0300

    .

---
+++ react-login/backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const routes = express.Router();
+const users = [{
+    id: 1,
+    name: 'John Doe',
+    email: 'contato@bitloop.com.br',
+    password: '123456',
+}]
+
+routes.post('/login', (req, res) => {
+    const { email, password } = req.body; 
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        return res.status(200).json(user);
+  
+    } else {
+        return res.status(401).send('Invalid credentials');
+    }
+});
+
+module.exports = routes;
--- react-login/backend/src/server.js
+++ react-login/backend/src/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const routes = require('./routes'); // Assuming routes.js is in the same directory
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { getAllUsers, createUser, updateUser, deleteUser } = require('./controllers/userController');    
+
+app.use(express.json());
+app.use(routes); // Mount the routes at /api
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+}); 
